63631072e8ba7c9739824da617d846daf52b230e
Kevin <kevindemaria95@gmail.com>
2024-01-10 17:31:27 +0530

    remove box component from chakra ui

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { HStack, Image, Box, Text } from '@chakra-ui/react';
+import { HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 
 const NavBar = () => {
