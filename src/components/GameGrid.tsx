ca006ab2566740c5ba370a30cf679cea31812f0c
Kevin <kevindemaria95@gmail.com>
2024-01-11 11:52:51 +0530

    add custom useFetch hook and integrate it

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,37 +1,45 @@
 import { useEffect, useState } from 'react';
 import apiClient, { AxiosError } from '../services/apiClient';
 import { Text } from '@chakra-ui/react';
+import useFetch from '../hooks/useFetch';
 
-interface Game {
+export interface Game {
   id: number;
   name: string;
 }
 
-interface FetchGamesResponse {
+export interface FetchGamesResponse {
   count: number;
   results: Game[];
 }
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState('');
+  const [games, isLoading, error] = useFetch();
 
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>('/games')
-      .then((res) => {
-        setGames(res.data.results);
-      })
-      .catch((err) => {
-        setError((err as AxiosError).message);
-      });
-  }, []);
+  //   const [games, setGames] = useState<Game[]>([]);
+  //   const [error, setError] = useState('');
+
+  //   useEffect(() => {
+  //     apiClient
+  //       .get<FetchGamesResponse>('/games')
+  //       .then((res) => {
+  //         setGames(res.data.results);
+  //       })
+  //       .catch((err) => {
+  //         setError((err as AxiosError).message);
+  //       });
+  //   }, []);
 
   return (
     <>
       {error && (
         <Text color='red' fontSize='large'>
           {error}
+        </Text>
+      )}
+      {isLoading && (
+        <Text color='blue' fontSize='xx-large'>
+          Loading...
         </Text>
       )}
       <ul>
